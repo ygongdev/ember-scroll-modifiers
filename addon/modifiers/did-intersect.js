d8e4496568fcd2cda063bcfe259ff500a565a4c2
Yicheng Gong <yigong@yigong-mn4.linkedin.biz>
2021-01-08 17:02:31 -0800

    Adding support for maxEnter and maxExit

--- addon/modifiers/did-intersect.js
+++ addon/modifiers/did-intersect.js
@@ -1,5 +1,6 @@
 import Modifier from 'ember-modifier';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { assert } from '@ember/debug';
 
 export const DEFAULT_OBSERVER_OPTIONS = {};
@@ -12,6 +13,13 @@ export default class DidIntersectModifier extends Modifier {
 
   // Observer options need to be specified, so the intersection observer admin service can use it as a key to unobserve, in case there is multiple observers on the same element. By default, we're using the same options that intersection observer uses.
   _options = DEFAULT_OBSERVER_OPTIONS;
+
+  // Used to track number of enter and exit intersections that have happened.
+  @tracked
+  _numOfEnters = 0;
+
+  @tracked
+  _numOfExits = 0;
 
   observe() {
     if (!this._isObservable) {
@@ -30,6 +38,7 @@ export default class DidIntersectModifier extends Modifier {
   }
 
   didUpdateArguments() {
+    this._resetIntersectionCounters();
     this.unobserve();
   }
 
@@ -38,15 +47,38 @@ export default class DidIntersectModifier extends Modifier {
       return;
     }
 
-    let { onEnter, onExit, options } = this.args.named;
+    let { onEnter, onExit, maxEnter, maxExit, options } = this.args.named;
 
     assert('onEnter or/and onExit is required', onEnter || onExit);
 
-    if (onEnter) {
-      this.observerManager.addEnterCallback(this.element, onEnter);
+    // We should technically accept 0, in case of programmatic changes
+    if (maxEnter || maxEnter === 0) {
+      assert('maxEnter must be an integer', Number.isInteger(maxEnter));
+      this._maxEnterIntersections = maxEnter;
     }
-    if (onExit) {
-      this.observerManager.addExitCallback(this.element, onExit);
+
+    // We should accept 0 here too.
+    if (maxExit || maxExit === 0) {
+      assert('maxExit must be an integer', Number.isInteger(maxExit));
+      this._maxExitIntersections = maxExit;
+    }
+
+    if (onEnter && maxEnter !== 0) {
+      this.observerManager.addEnterCallback(this.element, (...args) => {
+        if (!maxEnter || this._numOfEnters < maxEnter) {
+          onEnter(...args);
+        }
+        this._numOfEnters++;
+      });
+    }
+
+    if (onExit && maxExit !== 0) {
+      this.observerManager.addExitCallback(this.element, (...args) => {
+        if (!maxExit || this._numOfExits < maxExit) {
+          onExit(...args);
+        }
+        this._numOfExits++;
+      });
     }
 
     if (options) {
@@ -59,5 +91,10 @@ export default class DidIntersectModifier extends Modifier {
   // Move to willDestroy when we want to drop support for versions below ember-modifier 2.x
   willRemove() {
     this.unobserve();
+  }
+
+  _resetIntersectionCounters() {
+    this._numOfEnters = 0;
+    this._numOfExits = 0;
   }
 }
